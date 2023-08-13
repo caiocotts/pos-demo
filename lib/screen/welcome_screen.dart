8561b69862b5fb71c43a62b2ed07e6ab04db942f
Caio Cotts <caio@cotts.com.br>
2023-08-12 21:56:16 -0400

    Create welcome screen.

---
+++ lib/screen/welcome_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(child: Container()),
+                const Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      'Welcome to Cotts POS!',
+                      style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                const Text(
+                  'To begin the purchasing process, press the "New Purchase" button below.',
+                  textAlign: TextAlign.center,
+                ),
+                Expanded(child: Container()),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 60, left: 10, right: 10),
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                    child: const Text(
+                      'New Purchase',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
